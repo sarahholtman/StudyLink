c861cae7ffb6b613e36aa7450d73e9919ee29a93
sarahholtman <holt0064@algonquinlive.com>
2026-05-08 08:47:49 -0400

    Initial Spring Boot project setup

---
+++ studylink/src/main/java/com/studylink/StudylinkApplication.java
@@ -0,0 +1,13 @@
+package com.studylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudylinkApplication.class, args);
+	}
+
+}
---
+++ studylink/src/test/java/com/studylink/StudylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
